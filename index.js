894725a707b4445f620a8a7f5286339360747140
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-25 01:30:17 +0500

    fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const db = require("./models");
+require("dotenv").config();
 
 const authRouter = require("./routes/Auth");
 const userRouter = require("./routes/Users");
@@ -12,7 +13,9 @@ app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log("Server on port 3001");
     });
+}).catch((err) => {
+    console.log(err);
 });
