7711e31ad82f9c56fd2d66bf948f4e86ca92050c
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-25 04:58:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require("./models");
+const cors = require("cors");
 require("dotenv").config();
 
-const cors = require("cors");
-app.use(cors());
-
 app.use(express.json());
+app.use(cors());
 
 const authRouter = require("./routes/Auth");
 const userRouter = require("./routes/Users");
