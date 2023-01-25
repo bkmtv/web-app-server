bb39479ea17233ae7dd03d0f540e4d698d66adce
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-26 01:15:20 +0500

    Update Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -15,20 +15,29 @@ router.get("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
     const id = req.params.id;
+    const user = await Users.findByPk(id);
     await Users.destroy({ where: { id: id } });
-    res.json("deleted");
+    user.save();
+    const users = await Users.findAll();
+    res.send(users);
 });
 
 router.put("/:id", async (req, res) => {
     const id = req.params.id;
+    const user = await Users.findByPk(id);
     await Users.update({ status: 0 }, { where: { id: id } });
-    res.json("blocked");
+    user.save();
+    const users = await Users.findAll();
+    res.send(users);
 });
 
 router.patch("/:id", async (req, res) => {
     const id = req.params.id;
+    const user = await Users.findByPk(id);
     await Users.update({ status: 1 }, { where: { id: id } });
-    res.json("ublocked");
+    user.save();
+    const users = await Users.findAll();
+    res.send(users);
 });
 
 module.exports = router;
